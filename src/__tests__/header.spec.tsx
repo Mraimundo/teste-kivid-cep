cdb4b39d50dbc8b818968aa4522fae3752f4fdd2
Mraimundo <mdumundo18@gmail.com>
2024-02-26 12:49:13 -0300

    fix: fix eslint error in tests

--- src/__tests__/header.spec.tsx
+++ src/__tests__/header.spec.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { Header } from "../components/header";
@@ -11,8 +12,9 @@ const userMock = {
 describe("Header Component", () => {
   it("should renders header with correct user information", () => {
     const { getByText } = render(<Header />);
-
+    // @ts-ignore
     expect(getByText(userMock.author)).toBeInTheDocument();
+    // @ts-ignore
     expect(getByText(userMock.role)).toBeInTheDocument();
   });
 });
